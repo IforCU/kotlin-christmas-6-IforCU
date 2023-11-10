70865755bafdec27613fa3d75ebe6c1c9647fec9
I4CU <129958775+IforCU@users.noreply.github.com>
2023-11-10 14:32:29 +0900

    메뉴 enum 정의

--- src/main/java/christmas/Menu.java
+++ src/main/java/christmas/Menu.java
@@ -1,4 +1,39 @@
 package christmas;
 
 public enum Menu {
+    MushroomSoup("양송이스프", 6000, "Appetizer"),
+    Tapas("타파스", 5500, "Appetizer"),
+    CaesarSalad("시저샐러드", 8000, "Appetizer"),
+    TBoneSteak("티본스테이크", 55000, "Main"),
+    BBQRibs("바비큐립", 54000, "Main"),
+    SeafoodPasta("해산물파스타", 35000, "Main"),
+    ChristmasPasta("크리스마스파스타", 25000, "Main"),
+    ChocolateCake("초코케이크", 15000, "Dessert"),
+    IceCream("아이스크림", 5000, "Dessert"),
+    ZeroCola("제로콜라", 3000, "Beverage"),
+    RedWine("레드와인", 60000, "Beverage"),
+    Champagne("샴페인", 25000, "Beverage");
+
+    private final String menuname;
+    private final Integer price;
+    private final String kind;
+
+
+    Menu(String menuname, Integer price, String kind) {
+        this.menuname = menuname;
+        this.price = price;
+        this.kind = kind;
+    }
+
+    public String menuname() {
+        return menuname;
+    }
+
+    public Integer price() {
+        return price;
+    }
+
+    public String kind() {
+        return kind;
+    }
 }
